2cce4293e8ae00a848389b1cf2fe61ff83b271ed
manojkumarg7 <manojkumargowdru7@gmail.com>
2024-06-10 22:57:06 +0530

    code added

--- array/sort/programOne.js
+++ array/sort/programOne.js
@@ -6,5 +6,16 @@
 
 // To sort numbers properly, you can provide a comparison function to sort():
 
+// sort converts elemnts into strings
 const a = [23, 44, 56, 1, 2, 8];
 console.log(a.sort((a, b) => a - b).reverse());
+
+// slice
+// it creates new array doesnot remove any elements
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1);
+
+// splice
+// Splice(start ,remove ,adding new elemnts)
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+fruits1.splice(2, 0, "Lemon", "Kiwi");
