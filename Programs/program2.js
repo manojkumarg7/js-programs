185e451b0228fa1278413135a1d608e37279c524
manojkumarg7 <manojkumargowdru7@gmail.com>
2024-07-24 22:38:44 +0530

    added  code

---
+++ Programs/program2.js
@@ -0,0 +1,12 @@
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+const exampleObject = {
+  key1: "Geeks",
+  key2: 100,
+  key3: "Javascript",
+};
+
+ans = getKeyByValue(exampleObject, "Geeks");
+console.log(ans);
